ceee83cc5ebbd509866a5a4b2ce75fca842eb50b
mdnurakmal <mdnurakmal@gmail.com>
2022-02-04 11:12:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ueh1ta
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ueh1ta)
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+@NgModule({
+  imports: [CommonModule],
+  declarations: [SideBarComponent],
+})
+export class HomeModule {}
